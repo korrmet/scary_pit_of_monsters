ef6436107d782bc8b50bf7445a57db88b48c3d46
Korrmet <cyka.blyat.ebat.moy.huy@gmail.com>
2020-11-03 15:32:16 +0300

    delete object, fix(not at all)

--- c_oop/c_oop.c
+++ c_oop/c_oop.c
@@ -6,7 +6,7 @@
 void counter_operate(void* _this)
 { if (_this == NULL) { return; }
   THIS->_private.count++;
-  if (THIS->_public.delimeter >= THIS->_private.count)
+  if (THIS->_private.count >= THIS->_public.delimeter)
   { THIS->_private.count = 0;
     THIS->_public.state = (THIS->_public.state) ? 0 : 1;
   }
@@ -19,7 +19,7 @@ void predef_counter_operate(void* _this)
 { if (_this == NULL) { return; }
   if (THIS->_public.pre_load) { THIS->_public.pre_load--; return; }
   THIS->_private.count++;
-  if (THIS->_public.cnt.delimeter >= THIS->_private.count)
+  if (THIS->_private.count >= THIS->_public.cnt.delimeter)
   { THIS->_private.count = 0;
     THIS->_public.cnt.state = (THIS->_public.cnt.state) ? 0 : 1;
   }
--- c_oop/main.c
+++ c_oop/main.c
@@ -10,13 +10,16 @@ counter_t* ctr_pool[] = { (counter_t*)&ctr1__class,
                           (counter_t*)&ctr3__class };
 
 int main(void)
-{ ctr1->delimeter = 4;
+{ printf("visualization of three counter objects:\n");
+  ctr1->delimeter = 4;
   ctr2->pre_load = 3; ctr2->cnt.delimeter = 4;
   ctr3->delimeter = 2;
   for (int i = 0; i < 16; i++)
   { printf("%2d: ", i);
     for (int j = 0; j < 3; j++)
-    { ctr_pool[j]->operate(ctr_pool[j]); printf("%s", ctr_pool[j]->state); }
+    { ctr_pool[j]->operate(ctr_pool[j]); 
+      printf("%s", (ctr_pool[j]->state) ? "*" : " "); 
+    }
     printf("\n");
   }
 }
