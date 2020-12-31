8a878a7bf9107eb187b352b3d7525891e743dbff
Korrmet <cyka.blyat.ebat.moy.huy@gmail.com>
2020-12-31 18:28:49 +0300

    started writing data streamer doxygen comments

--- infinite_int/infint/tests/data_streamer.h
+++ infinite_int/infint/tests/data_streamer.h
@@ -10,7 +10,21 @@
 #define STREAM_WR_MODE__INSERT  0
 #define STREAM_WR_MODE__REPLACE 1
 
+/** \brief  read from data stream
+ *  \param  ptr pointer to data buffer which be used to read
+ *  \param  len length of data will be readen
+ *  \param  id  id of data stream
+ *  \return num of actually readen bytes or error sequence
+ *  \retval -1  error occured
+ *  \retval >=0 num of actually readen bytes */
 int rd(void* ptr, unsigned int len, unsigned int id);
+
+/** \brief  write to data stream
+ *  \param  ptr pointer to data must be written in stream
+ *  \param  len number of bytes which must be written in stream
+ *  \param  mode mode of write. can be STREAM_WR_MODE__INSERT or 
+ *               STREAM_WR_MODE__REPLACE
+ *  \param  id  identifier of data stream */
 int wr(void* ptr, unsigned int len, int mode, unsigned int id);
 int rm(int len, int id);
 int pos(int pos, int shift, unsigned int id);
